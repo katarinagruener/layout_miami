ff9d90b063fdfb44a9a8db740cddb97e126cfc4d
Katarina <katarinagruener@gmail.com>
2023-03-19 11:58:22 +0100

    miami design complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+window.addEventListener('hashchange', function() {
+  if (this.window.location.hash === '#menu') {
+    this.document.body.classList.add('page__body--with-menu');
+  } else {
+    this.document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+/*
+function myFunc() {} // function declaration; myFunc()
+const myFunc1 = function() {}; // function expression; myFunc1()
+const arrowFunction = () => {}; */
+
+/*
+(function(x) {
+    console.log(x);
+})(5) */
+/* ((x) => {
+    console.log(x);
+})(5) */
